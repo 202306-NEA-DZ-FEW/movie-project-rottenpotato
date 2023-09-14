9151e23f0f9094ea5aa8c28a35d6e89dae71fff6
AMEL BATOUCHE <amelbat7400@gmail.com>
2023-09-14 16:36:18 +0200

    feat(component): cretaing and styling a navbar
    NavBar Component #5

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
-import "@/styles/globals.css"
+import NavBar from "../components/NavBar"
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// import "@/styles/globals.css"
+import "@/styles/globals.css"
+function MyApp({ Component, pageProps }) {
+  return (
+    <div>
+      <NavBar />
+      <Component {...pageProps} />
+    </div>
+  )
 }
+
+export default MyApp
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
-import Image from "next/image"
+import React from "react"
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 `}
-    >
-      <h1 className="text-7xl font-extrabold text-yellow-300">
-        Rotten potato rocks
-      </h1>
-    </main>
+    <div className="bg-black">
+      <main
+        className={`flex min-h-screen flex-col items-center justify-between p-24 `}
+      >
+        <h1 className="text-7xl font-extrabold text-yellow-300">
+          Rotten potato rocks
+        </h1>
+      </main>
+    </div>
   )
 }
