03bfd5bb9090fdf5cacd5dabd3af5e0a484c8acb
GhaniBahri <bahri.ghanou@gmail.com>
2023-09-17 23:45:54 +0200

    feat(file): merge dark mode

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Home({}) {
   return (
-    <div className="bg-black">
+    <div className="bg-white dark:black text-black dark:text-white">
       <main
         className={`flex min-h-screen flex-col items-center justify-between p-24 `}
       >
